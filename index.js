54419fd9cf8eb9de7e03863fa677f1f2bbc915db
BakiAbdullah <shatil605@gmail.com>
2023-05-16 13:12:53 +0600

    ema-jhon server pagination applied

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.n0k7opi.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n0k7opi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +24,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db("emaJohnDB").collection("products");
+
+    // Get Operation
+    app.get("/products", async (req, res) => {
+      console.log(req.query);
+      const page = parseInt(req.query.page) || 0;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = page * limit
+      const result = await productCollection.find().skip(skip).limit(limit).toArray();
+      res.send(result);
+    });
+
+    // End Point for Estimated Count in a Database Collection
+    app.get("/totalProducts", async (req, res) => {
+      const result = await productCollection.estimatedDocumentCount();
+      res.send({ totalProducts: result });
+    });
+
+    app.post('/productsByIds', async(req,res)=>{
+      const ids = req.body;
+      const objectIds = ids.map(id=> new ObjectId(id));
+
+      const query = { _id: { $in: objectIds } };
+      console.log(ids)
+      const result = await productCollection.find(query).toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,16 +60,15 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("John is Busy With Shopping!");
+});
 
-app.get('/', (req,res)=>{
-  res.send('John is Busy With Shopping!')
-})
-
-app.listen(port, ()=>{
-  console.log(`Ema-john Server is Running on Port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Ema-john Server is Running on Port: ${port}`);
+});
